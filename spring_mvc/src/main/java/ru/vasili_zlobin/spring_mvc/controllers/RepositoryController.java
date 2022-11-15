45e56c2269fd6ec222e6f91444f5fa7aeb0eb562
Vasili Zlobin <vaszlbn@gmail.com>
2022-11-15 20:13:11 +0300

    add form in spring_mvc

--- spring_mvc/src/main/java/ru/vasili_zlobin/spring_mvc/controllers/RepositoryController.java
+++ spring_mvc/src/main/java/ru/vasili_zlobin/spring_mvc/controllers/RepositoryController.java
@@ -2,10 +2,7 @@ package ru.vasili_zlobin.spring_mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import ru.vasili_zlobin.spring_mvc.model.Product;
 import ru.vasili_zlobin.spring_mvc.model.ProductRepository;
 
@@ -30,6 +27,19 @@ public class RepositoryController {
             result = getListProduct().get(id).toString();
         }
         return String.format("<h1>%s</h1>", result);
+    }
+
+    @GetMapping("/form_add")
+    public String viewFormAdd() {
+        return "product_add";
+    }
+
+    @GetMapping("/product_add")
+    public String addProduct(@RequestParam(defaultValue = "") String title, @RequestParam(defaultValue = "-1") Double cost) {
+        if (!title.isEmpty() && cost > 0) {
+            ProductRepository.getInstance().addProduct(title, cost);
+        }
+        return "redirect:/products";
     }
 
     private List<Product> getListProduct() {
