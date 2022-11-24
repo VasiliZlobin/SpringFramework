7705640a2ce7d9c2921229d436c6b642637b2cc5
Vasili Zlobin <vaszlbn@gmail.com>
2022-11-24 21:56:28 +0300

    add solution task lesson5

---
+++ hibernate/src/main/java/ru/vasili_zlobin/hibernate/model/Product.java
@@ -0,0 +1,52 @@
+package ru.vasili_zlobin.hibernate.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String title, Double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s) %s, Стоимость: %s", id, title, price);
+    }
+}
